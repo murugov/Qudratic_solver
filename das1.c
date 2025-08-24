72c62ef4573e6fbd0ea7b1408adb42f978086e48
murugov2007 <murugov2007@yandex.ru>
2025-08-24 20:38:35 +0300

    Update das1.c

--- das1.c
+++ das1.c
@@ -6,22 +6,20 @@
 
 void* append(double* ptr_data, int* ptr_length, int* ptr_capacity)
 {
-    if(*ptr_length >= *ptr_capacity)
-    {
-        *ptr_capacity = *ptr_capacity * 2;
-        double* ptr_list = (double *)calloc(*ptr_capacity, sizeof(double));
-        if(ptr_list == NULL)
-            return ptr_data;
-
-        for(int i = 0;i < *ptr_length; ++i)
-            ptr_list[i] = ptr_data[i];
+    *ptr_capacity = *ptr_capacity * 2;
+    double* ptr_list = (double *)calloc(*ptr_capacity, sizeof(double));
+    if(ptr_list == NULL)
+        return ptr_data;
+        
+    for(int i = 0;i < *ptr_length; ++i)
+        ptr_list[i] = ptr_data[i];
         
         free(ptr_data);
         ptr_data = ptr_list;
-    }
-    
+        
     return ptr_data;
 }
+
 
 int main(void)
 {
@@ -39,25 +37,25 @@ int main(void)
         return 1;
     }
 
-    char nan_proof[5];
+    char nan_proof[4];
 
     while(1)
     {
-        if(fscanf(ptr_file, "%lg, ", ptr_data + length) == 1)
-        {
+        if (length == capacity) 
             ptr_data = append(ptr_data, &length, &capacity);
+
+        if (fscanf(ptr_file, "%lg ", ptr_data + length) == 1)
+        {
+            printf("[%d]> number: %lg\n", length, ptr_data[length]);
             length++;
-            printf("999\n");
-            continue;
         }
-        // else if(fscanf(ptr_file, "%s ", nan_proof) == 1 && !strcmp(nan_proof, "NAN"))
-        // {
-        //     printf("777\n");
-        //     ptr_data[length] = NAN;
-        //     ptr_data = append(ptr_data, &length, &capacity);
-        //     length++;
-        //     continue;
-        //        }
+        else if (fscanf(ptr_file, "%s ", nan_proof) == 1 && !strcmp(nan_proof, "NAN"))
+        {
+            ptr_data[length] = NAN;
+            printf("[%d]> number: %lg\n", length, ptr_data[length]);
+            length++;
+
+        }
         else
         {
             break;
