7992e7c0f8d92364a24504d2d7444e2c874d81e6
murugov2007 <murugov2007@yandex.ru>
2025-08-24 18:18:21 +0300

    Update das1.c

--- das1.c
+++ das1.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 void* append(double* ptr_data, int* ptr_length, int* ptr_capacity)
 {
-    if(*ptr_length > *ptr_capacity)
+    if(*ptr_length >= *ptr_capacity)
     {
         *ptr_capacity = *ptr_capacity * 2;
-        double* ptr_list = calloc(sizeof(double) * *ptr_capacity, sizeof(double));
+        double* ptr_list = (double *)calloc(*ptr_capacity, sizeof(double));
         if(ptr_list == NULL)
             return ptr_data;
+
         for(int i = 0;i < *ptr_length; ++i)
             ptr_list[i] = ptr_data[i];
         
         free(ptr_data);
         ptr_data = ptr_list;
     }
-    (*ptr_length)++;
     
     return ptr_data;
 }
@@ -26,18 +27,41 @@ int main(void)
 {
     int capacity = 5;
     int length = 0;
-    double* ptr_data = calloc(sizeof(double) * capacity, sizeof(double));
- 
+    double* ptr_data = (double *)calloc(capacity, sizeof(double));
+    if (!ptr_data) {
+        perror("> error while allocating memory\n");
+        return 1;
+    }
+
     FILE* ptr_file = fopen("test_input.txt", "r");
     if(ptr_file == NULL) {
         perror("test_input.txt");
         return 1;
     }
 
-    while(fscanf(ptr_file, "%lg ", &ptr_data[length]) == 1)
+    char nan_proof[5];
+
+    while(1)
     {
-        printf("%lg\n", ptr_data[length]);
-        append(ptr_data, &length, &capacity);
+        if(fscanf(ptr_file, "%lg, ", ptr_data + length) == 1)
+        {
+            ptr_data = append(ptr_data, &length, &capacity);
+            length++;
+            printf("999\n");
+            continue;
+        }
+        // else if(fscanf(ptr_file, "%s ", nan_proof) == 1 && !strcmp(nan_proof, "NAN"))
+        // {
+        //     printf("777\n");
+        //     ptr_data[length] = NAN;
+        //     ptr_data = append(ptr_data, &length, &capacity);
+        //     length++;
+        //     continue;
+        //        }
+        else
+        {
+            break;
+        }
     }
 
     for(int i = 0; i < length; ++i)
