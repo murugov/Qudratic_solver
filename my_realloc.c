a54281d3aa65ded87f596e30cfe3e8effd1f025b
murugov2007 <murugov2007@yandex.ru>
2025-08-25 13:04:03 +0300

    Create my_realloc.c

---
+++ my_realloc.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// void* my_realloc(double* ptr_data, int* ptr_length, int* ptr_capacity)
+// {
+//     *ptr_capacity = *ptr_capacity * 2;
+//     double* ptr_list = (double *)calloc(*ptr_capacity, sizeof(double));
+//     if(ptr_list == NULL)
+//         return ptr_data;
+        
+//     for(int i = 0;i < *ptr_length; ++i)
+//         ptr_list[i] = ptr_data[i];
+        
+//         free(ptr_data);
+//         ptr_data = ptr_list;
+        
+//     return ptr_data;
+// }
+
+
+int main(void)
+{
+    int capacity = 5;
+    int length = 0;
+    double* ptr_data = (double *)calloc(capacity, sizeof(double));
+    if (!ptr_data) {
+        return 1;
+    }
+
+    FILE* ptr_file = fopen("test_input.txt", "r");
+    if(ptr_file == NULL) {
+        perror("test_input.txt");
+        return 1;
+    }
+
+    char nan_proof[4];
+
+    while(1)
+    {
+        if (length == capacity) 
+        {
+            //ptr_data = my_realloc(ptr_data, &length, &capacity);
+            capacity *= 2;
+            ptr_data = (double *)realloc(ptr_data, sizeof(double) * capacity);
+        }
+
+
+        if (fscanf(ptr_file, "%lg ", &ptr_data[length]) == 1)
+        {
+            length++;
+        }
+        // else if (fscanf(ptr_file, "%s ", nan_proof) == 1 && !strcmp(nan_proof, "NAN"))
+        // {
+        //     ptr_data[length] = NAN;
+        //     length++;
+
+        // }
+        else
+        {
+            break;
+        }
+    }
+
+    for(int i = 0; i < length; ++i)
+        printf("%lg ", ptr_data[i]);
+ 
+    fclose(ptr_file);
+ 
+    return 0;
+}
+
+
